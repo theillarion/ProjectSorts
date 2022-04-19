fda682aa1bdb128ff537e924e55ed90691bfce7d
Illarion <pro100illarion@gmail.com>
2022-04-19 21:07:24 +0300

    Random

---
+++ includes/generate_random.hpp
@@ -0,0 +1,33 @@
+//
+// Created by Illarion on 19.04.2022.
+//
+
+#ifndef PROJECTSORTS_GENERATE_RANDOM_HPP
+#define PROJECTSORTS_GENERATE_RANDOM_HPP
+
+# include <vector>
+# include <random>
+
+int	GenerateRandomInt32(int min, int max)
+{
+	std::random_device				rnd;
+	std::mt19937					gen(rnd());
+	std::uniform_int_distribution<int>	uniform(min, max);
+	return (uniform(gen));
+}
+
+std::vector<int>	GenerateRandomVector(int min, int max, size_t	size)
+{
+	std::vector<int>				result;
+	std::random_device				rnd;
+	std::mt19937					gen(rnd());
+	std::uniform_int_distribution<>	uniform(min, max);
+
+	result.reserve(size);
+	for (auto i = 0; i < size; ++i)
+		result.push_back(uniform(gen));
+
+	return (result);
+}
+
+#endif //PROJECTSORTS_GENERATE_RANDOM_HPP
