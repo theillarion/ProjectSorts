11d4100bf3726fd09675e1d511d0d7dda6e0ec3a
Illarion <pro100illarion@gmail.com>
2022-04-22 21:00:05 +0300

    Deleted extra code

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -9,33 +9,20 @@
 
 int main()
 {
-	std::vector<int>	numbers = GenerateRandomVector(INT32_MIN, INT32_MAX, 1000);
-	TestIsSorted("Insertion sort", my_sort::InsertionSort<std::vector<int>::iterator>);
-	//TestIsSorted("Quick sort", my_sort::InsertionSort<std::vector<int>::iterator>, 100, 10000);
-	////Test1();
-	/*TestTime("Bubble sort", my_sort::BubbleSort<std::vector<int>::iterator>, numbers);
-	TestTime("Shaker sort", my_sort::ShakerSort<std::vector<int>::iterator>, numbers);
-	TestTime("Odd-Even sort", my_sort::OddEvenSort<std::vector<int>::iterator>, numbers);
-	TestTime("Comb sort", my_sort::CombSort<std::vector<int>::iterator>, numbers);
-	TestTime("Selection sort", my_sort::SelectionSort<std::vector<int>::iterator>, numbers);
-	TestTime("Insertion sort", my_sort::InsertionSort<std::vector<int>::iterator>, numbers);
-	TestTime("Quick sort", my_sort::QuickSort<std::vector<int>::iterator>, numbers);*/
+	std::vector<int>	numbers = GenerateRandomVector(INT32_MIN, INT32_MAX, 500000000);
 
-	//std::vector<int>	numbers2{ 8, 3, 2, 7, 9, 1, 4 };		//{3, 1, 2, 4, 1, 5, 9, 2};
-
-	//my_sort::HeapSort(numbers2);
-
-	int* arr = new int[] {1, 2, 4, 0, 0};
-
-	for (int i = 0; i < 100; i++)
-	{
-		my_sort::QuickSort(arr, arr + 5);
-
-	}
-
-	//TestForArray();
-
-	//std::cout << numbers2;
-
+	TestTime("Quick sort", my_sort::QuickSort<std::vector<int>::iterator>, numbers);
+	TestTime("Original sort", std::sort<std::vector<int>::iterator>, numbers);
 	return 0;
 }
+
+
+//TestIsSorted("Quick sort", my_sort::QuickSort<std::vector<int>::iterator>);
+//TestIsSorted("Quick sort", my_sort::QuickSort<std::vector<int>::iterator>, 100, 10000);
+////Test1();
+/*TestTime("Bubble sort", my_sort::BubbleSort<std::vector<int>::iterator>, numbers);
+TestTime("Shaker sort", my_sort::ShakerSort<std::vector<int>::iterator>, numbers);
+TestTime("Odd-Even sort", my_sort::OddEvenSort<std::vector<int>::iterator>, numbers);
+TestTime("Comb sort", my_sort::CombSort<std::vector<int>::iterator>, numbers);
+TestTime("Selection sort", my_sort::SelectionSort<std::vector<int>::iterator>, numbers);
+TestTime("Insertion sort", my_sort::InsertionSort<std::vector<int>::iterator>, numbers);*/
