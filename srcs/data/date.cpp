211a9dc4b9f861f8c5e152e05e4651367d8cd1e4
Illarion <pro100illarion@gmail.com>
2022-04-27 20:09:58 +0300

    Changed initialization of arguments

--- srcs/data/date.cpp
+++ srcs/data/date.cpp
@@ -3,17 +3,16 @@
 //
 #include "data/date.hpp"
 
-Date::Date(unsigned short day, unsigned short month, unsigned short year):
+Date::Date(unsigned short day, unsigned short month, unsigned short year) :
 	day(day), month(month), year(year)
 {
 
 }
 
-Date::Date(const Date&	date_time)
+Date::Date(const Date&	date_time) :
+	day(date_time.day), month(date_time.month), year(date_time.year)
 {
-	day = date_time.day;
-	month = date_time.month;
-	year = date_time.year;
+
 }
 
 std::ostream&	operator<<(std::ostream&	out, const Date &date_time)
